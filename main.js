5c5bdcb2d8ba1bd79b11f02b1824703bf5ed979d
Shikha246 <73099321+Shikha246@users.noreply.github.com>
2021-09-12 15:05:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+var SpeechRecognition = window.webkitSpeechRecognition;// webspeechAPI
+  
+var recognition = new SpeechRecognition(); // new create the instance of the webspeechAPI
 objects = [];
 status = "";
 
@@ -31,30 +33,45 @@ function modelLoaded() {
   // video.volume(0);
 }
 
-// function gotResult(error, results) {
-//   if (error) {
-//     console.log(error);
-//   }
-//   console.log(results);
-//   objects = results;
-// }
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+function speak(){
+  var synth = window.speechSynthesis;
 
+  speak_data = object_name +"Object mentioned found";
+
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+  synth.speak(utterThis);
+
+  
+}
 
 function draw() {
   image(video, 0, 0, 480, 300);
-      // if(status != "")
-      // {
-      //   objectDetector.detect(video, gotResult);
-      //   for (i = 0; i < objects.length; i++) {
-      //     document.getElementById("status").innerHTML = "Status : Objects Detected";
-      //     document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
- 
-      //     fill("#FF0000");
-      //     percent = floor(objects[i].confidence * 100);
-      //     text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
-      //     noFill();
-      //     stroke("#FF0000");
-      //     rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-      //   }
-      // }
+      if(status != "")
+      {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Objects Detected";
+          document.getElementById("object_found").innerHTML = "Object Found : "+ object_name;
+          speak();
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
+      else{
+        
+          document.getElementById("object_found").innerHTML = "Object mentioned not Found  ";
+        
+      }
 }
